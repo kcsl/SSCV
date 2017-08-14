36d22c283f44feb8ce3b1c5cf1c4f13578428a60
Payas Awadhutkar <payasra@gmail.com>
2017-08-14 11:18:57 -0500

    First set of SSCV Loops

---
+++ SSCV-Loops/src/com/kcsl/sscv/Loop1_vulnerable.java
@@ -0,0 +1,44 @@
+package com.kcsl.sscv;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class Loop1_vulnerable {
+	
+	private static int MAX_INPUT = 500;
+	
+	private static BigInteger ONE = BigInteger.ONE;
+	
+	public static void main(int myInput) {
+		
+		BigInteger[] arr = new BigInteger[MAX_INPUT+1];
+		
+		for(int i = 0; i <= MAX_INPUT; i++) {
+			arr[i] = BigInteger.valueOf(i);
+		}
+		
+		BigInteger prime = BigInteger.probablePrime(127, new Random());
+		
+		for (int j = myInput; j <= MAX_INPUT; j++){
+            BigInteger d = differ(arr[j], MAX_INPUT / 2, prime, ONE);
+		}
+	}
+	
+	public static BigInteger differ(BigInteger a, int b, BigInteger n, BigInteger o) {
+		
+		if (b%2 == 1) {
+			return oddDiff(a, b, n, o);
+		}
+		if (b == 0) {
+	        return o;
+	    } else {
+	    	return oddDiff(a, b - 1, n, o.multiply(a).mod(n));
+		}
+		
+	}
+	
+	public static BigInteger oddDiff(BigInteger a, int b, BigInteger p, BigInteger curr) {
+		return differ(a, b - (b % 2), p, curr.multiply(a).mod(p));
+
+	}
+}
